168d005a8ec637e7e233cecbce7e4e2ac94ba26e
gbonneau <gbonneau@CMW1-GBONNEAU.rambus.com>
2022-11-24 08:17:20 -0500

    Statistic

--- contrib/corpus/corpuszlib.cpp
+++ contrib/corpus/corpuszlib.cpp
@@ -6,18 +6,22 @@
 #include <string>
 #include <memory>
 #include <map>
+#include <list>
+#include <iomanip>
 #include <cassert>
 
 
 //#define _CRTDBG_MAP_ALLOC
-//#include <stdlib.h>
-//#include <crtdbg.h>
-//
-//#ifdef _DEBUG
-//#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
-//#else
-//#define DBG_NEW new
-//#endif
+#ifdef _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#ifdef _DEBUG
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#else
+#define DBG_NEW new
+#endif
+#endif
 
 extern "C" {
 #include "zlib.h"
@@ -62,20 +66,26 @@ int __cdecl MyAllocHook(
 
 int main()
 {
-
-   //_CrtSetAllocHook(MyAllocHook);
-
-   std::shared_ptr<unsigned char> fileBuffer = std::shared_ptr<unsigned char>(new unsigned char[256]);
-   std::shared_ptr<unsigned char> deflateBuffer = std::shared_ptr<unsigned char>(new unsigned char[512]);
+#ifdef _CRTDBG_MAP_ALLOC
+   _CrtSetAllocHook(MyAllocHook);
+#endif
 
    int32_t huffStrategy[2] = { Z_FIXED, Z_DEFAULT_STRATEGY };
    uint32_t dataChunk[] = { 256,512,1024,2048,4096 };
 
-   std::string listFileName("C:\\Users\\gbonneau\\git\\zlib\\data\\listFile.txt");
+   std::list<std::shared_ptr<std::ifstream>> corpusList;
+   std::shared_ptr<unsigned char> fileBuffer = std::shared_ptr<unsigned char>(new unsigned char[4096]);
+   std::shared_ptr<unsigned char> deflateBuffer = std::shared_ptr<unsigned char>(new unsigned char[4096*2]);
+
+   //std::string listFileName("C:\\Users\\gbonneau\\git\\zlib\\data\\calgary_corpus.txt");
+   //std::string listFileName("C:\\Users\\gbonneau\\git\\zlib\\data\\enwik9.txt");
+   //std::string listFileName("C:\\Users\\gbonneau\\git\\zlib\\data\\silicia_corpus.txt");
+   std::string listFileName("C:\\Users\\gbonneau\\git\\zlib\\data\\canterbury_corpus.txt");
+   
+
    std::string srcPathFileName;
 
    std::ifstream listFile;
-   std::ifstream srcFile;
 
    listFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
 
@@ -87,23 +97,38 @@ int main()
       exit(-1);
    }
    
-   std::getline(listFile, srcPathFileName);
-   if (srcPathFileName.empty()) {
-      std::cout << "Filename from list file is empty" << std::endl;
-      exit(-2);
-   }
+   while (true) {
+      srcPathFileName.clear();
+      try {
+         std::getline(listFile, srcPathFileName);
+      }
+      catch(std::system_error& e) {
+         if (listFile.eof()) {
+            break;
+         }
+         std::cerr << e.code().message() << std::endl;
+         exit(-5);
+      }
+      std::shared_ptr<std::ifstream> srcFile = std::make_shared<std::ifstream>();
+      srcFile->exceptions(std::ifstream::failbit | std::ifstream::badbit | std::ifstream::eofbit);
+      corpusList.push_back(srcFile);
 
-   srcFile.open(srcPathFileName.c_str(), std::ios::in | std::ios::binary);
-   if (srcFile.fail()) {
-      std::cout << "Failed to open File " << listFileName << std::endl;
-      exit(-1);
+      try {
+         srcFile->open(srcPathFileName.c_str(), std::ios::in | std::ios::binary);
+      }
+      catch (std::system_error& e) {
+         std::cerr << e.code().message() << std::endl;
+         exit(-1);
+      }
    }
-   std::string srcFileName = srcPathFileName.substr(srcPathFileName.find_last_of("/\\") + 1);
+   listFile.close();
 
-   for (uint32_t chunckIndex = 3; chunckIndex < 5; chunckIndex++)
+   std::string srcStatName = listFileName.substr(listFileName.find_last_of("/\\") + 1);
+
+   for (uint32_t chunckIndex = 0; chunckIndex < 5; chunckIndex++)
    {
       std::shared_ptr<std::map<uint32_t, huffMethod>> compStatistic = std::shared_ptr<std::map<uint32_t, huffMethod>>(new std::map<uint32_t, huffMethod>);
-      std::string statFileName = std::string("deflate_") + srcFileName + "_" + std::to_string(dataChunk[chunckIndex]) + ".csv";
+      std::string statFileName = std::string("deflate_") + srcStatName + "_" + std::to_string(dataChunk[chunckIndex]) + ".csv";
 
       std::ofstream statFile;
       statFile.exceptions(std::ofstream::failbit | std::ofstream::badbit);
@@ -124,85 +149,100 @@ int main()
          (*compStatistic)[i].staticSize = 0;
          (*compStatistic)[i].dynamicSize = 0;
       }
-      srcFile.clear();
-      srcFile.seekg(0);
 
       uint64_t totalSizeRead = 0;
       uint64_t totalSizeCompress[2] = {};
       uint64_t totalMBytes = 0;
+      uint64_t lastMBytes = 0;
       uint64_t loopCount = 0;
 
-      while (true) {
-         srcFile.read((char*)fileBuffer.get(), dataChunk[chunckIndex]);
-         if (!srcFile) {
-            std::cout << "EOF reached " << srcFile.gcount() << " could be read" << std::endl;
-            break;
-         }
-         totalSizeRead += dataChunk[chunckIndex];
-         totalMBytes = totalSizeRead >> 20;
-         loopCount++;
+      for (auto iterFile = corpusList.begin(); iterFile != corpusList.end(); ++iterFile) {
 
-         unsigned char* buffer = fileBuffer.get();
-         int cprLevel = Z_DEFAULT_COMPRESSION;
+         std::shared_ptr<std::ifstream> srcFile = *iterFile;
+         srcFile->clear();
+         srcFile->seekg(0);
 
-         for (uint32_t huffIndex = 0; huffIndex < 2; huffIndex++) {
-
-            z_stream zcprDeflate;
-            int ret = Z_OK;
-            memset(&zcprDeflate, 0, sizeof(z_stream));
-
-            zcprDeflate.next_in = fileBuffer.get();
-            zcprDeflate.next_out = deflateBuffer.get();
-            zcprDeflate.avail_in = dataChunk[chunckIndex];
-            zcprDeflate.avail_out = dataChunk[chunckIndex] * 2;
-
-            zcprDeflate.zalloc = nullptr;
-            zcprDeflate.zfree = nullptr;
-            zcprDeflate.opaque = nullptr;
-
-            deflateInit2(&zcprDeflate, cprLevel, Z_DEFLATED, MAX_WBITS, MAX_MEM_LEVEL, huffStrategy[huffIndex]);
-            ret = deflate(&zcprDeflate, Z_FINISH);
-            deflateEnd(&zcprDeflate);
-
-            std::shared_ptr<unsigned char> inflateBuffer = std::shared_ptr<unsigned char>(new unsigned char[(dataChunk[chunckIndex])]);
-
-            z_stream zcprInflate;
-            ret = Z_OK;
-            memset(&zcprInflate, 0, sizeof(z_stream));
-
-            totalSizeCompress[huffIndex] += zcprDeflate.total_out;
-
-            zcprInflate.next_in = deflateBuffer.get();
-            zcprInflate.next_out = inflateBuffer.get();
-            zcprInflate.avail_in = zcprDeflate.total_out;
-            zcprInflate.avail_out = dataChunk[chunckIndex] * 2;
-
-            inflateInit(&zcprInflate);
-            ret = inflate(&zcprInflate, Z_FINISH);
-            inflateEnd(&zcprInflate);
-
-            int retCmp = std::strncmp((const char*)fileBuffer.get(), (const char*)inflateBuffer.get(), dataChunk[chunckIndex]);
-
-            if (retCmp != 0) {
-               assert(false);
+         while (true) {
+            try {
+               srcFile->read((char*)fileBuffer.get(), dataChunk[chunckIndex]);
+            }
+            catch (std::system_error& e) {
+               if (srcFile->eof()) {
+                  std::cout << std::endl;
+                  break;
+               }
+               std::cerr << e.code().message() << std::endl;
                exit(-4);
             }
-            huffStrategy[huffIndex] == Z_FIXED ? (*compStatistic)[zcprDeflate.total_out].staticSize++ : (*compStatistic)[zcprDeflate.total_out].dynamicSize++;
+            totalSizeRead += dataChunk[chunckIndex];
+            totalMBytes = totalSizeRead >> 20;
+            if (lastMBytes != totalMBytes) {
+               lastMBytes = totalMBytes;
+               std::cout << " \r MBytes Read = " << totalMBytes;
+            }
+            loopCount++;
+
+            unsigned char* buffer = fileBuffer.get();
+            int cprLevel = Z_DEFAULT_COMPRESSION;
+
+            for (uint32_t huffIndex = 0; huffIndex < 2; huffIndex++) {
+
+               z_stream zcprDeflate;
+               int ret = Z_OK;
+               memset(&zcprDeflate, 0, sizeof(z_stream));
+
+               zcprDeflate.next_in = fileBuffer.get();
+               zcprDeflate.next_out = deflateBuffer.get();
+               zcprDeflate.avail_in = dataChunk[chunckIndex];
+               zcprDeflate.avail_out = dataChunk[chunckIndex] * 2;
+
+               zcprDeflate.zalloc = nullptr;
+               zcprDeflate.zfree = nullptr;
+               zcprDeflate.opaque = nullptr;
+
+               deflateInit2(&zcprDeflate, cprLevel, Z_DEFLATED, MAX_WBITS, MAX_MEM_LEVEL, huffStrategy[huffIndex]);
+               ret = deflate(&zcprDeflate, Z_FINISH);
+               deflateEnd(&zcprDeflate);
+
+               std::shared_ptr<unsigned char> inflateBuffer = std::shared_ptr<unsigned char>(new unsigned char[(dataChunk[chunckIndex])]);
+
+               z_stream zcprInflate;
+               ret = Z_OK;
+               memset(&zcprInflate, 0, sizeof(z_stream));
+
+               totalSizeCompress[huffIndex] += zcprDeflate.total_out;
+
+               zcprInflate.next_in = deflateBuffer.get();
+               zcprInflate.next_out = inflateBuffer.get();
+               zcprInflate.avail_in = zcprDeflate.total_out;
+               zcprInflate.avail_out = dataChunk[chunckIndex] * 2;
+
+               inflateInit(&zcprInflate);
+               ret = inflate(&zcprInflate, Z_FINISH);
+               inflateEnd(&zcprInflate);
+
+               int retCmp = std::strncmp((const char*)fileBuffer.get(), (const char*)inflateBuffer.get(), dataChunk[chunckIndex]);
+
+               if (retCmp != 0) {
+                  assert(false);
+                  exit(-4);
+               }
+               huffStrategy[huffIndex] == Z_FIXED ? (*compStatistic)[zcprDeflate.total_out].staticSize++ : (*compStatistic)[zcprDeflate.total_out].dynamicSize++;
+            }
          }
       }
 
       for (uint32_t i = 1; i < (dataChunk[chunckIndex] + 16); i++) {
          std::cout << "compression size = " << i << ", static count = " << (*compStatistic)[i].staticSize << ", dynamic count = " << (*compStatistic)[i].dynamicSize << std::endl;
-         statFile << i << "," << (*compStatistic)[i].staticSize << "," << (*compStatistic)[i].dynamicSize << std::endl;
+         statFile << std::fixed << std::setprecision(2) << i << "," << ((double)dataChunk[chunckIndex] / (double)i) << "," << (*compStatistic)[i].staticSize << "," << (*compStatistic)[i].dynamicSize << std::endl;
       }
-      std::cout << "Compression ratio static Huffman = " << (double)totalSizeRead / (double)totalSizeCompress[0] << " dynamic Huffman = " << (double)totalSizeRead / (double)totalSizeCompress[1] << std::endl;
+      std::cout << "Compression ratio static Huffman = " << (double)totalSizeRead / (double)totalSizeCompress[0] << " dynamic Huffman = " << (double)totalSizeRead / (double)totalSizeCompress[1] << std::endl << std::endl;
       statFile << std::endl << "Compression ratio static Huffman = " << (double)totalSizeRead / (double)totalSizeCompress[0] << " dynamic Huffman = " << (double)totalSizeRead / (double)totalSizeCompress[1] << std::endl << std::endl;;
       statFile.close();
    }
-
-
-   listFile.close();
-   srcFile.close();
-
+   for (auto iterFile = corpusList.begin(); iterFile != corpusList.end(); ++iterFile) {
+      std::shared_ptr<std::ifstream> srcFile = *iterFile;
+      srcFile->close();
+   }
 }
 
